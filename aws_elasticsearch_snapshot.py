2142aa57d42a1bee67fd070d602cfc42af9e31a9
José Armesto <github@armesto.net>
2016-01-27 13:15:19 +0100

    Module can take snapshots too

--- aws_elasticsearch_snapshot.py
+++ aws_elasticsearch_snapshot.py
@@ -20,10 +20,12 @@
 
 DOCUMENTATION = """
 ---
-module: register_snapshot_repository
-short_description: Registers a Snapshot Directory for ElasticSearch Snapshots
+module: aws_elasticsearch_snapshot
+short_description: Registers a Snapshot Directory, or take snapshots for ElasticSearch Snapshots.
 description:
   - It depends on boto
+  - If bucket and role_arn are specified, snapshot repository will be created.
+  - If snapshot name is specified, snapshot will be taken.
 
 version_added: "2.1"
 author: "Jose Armesto (@fiunchinho)"
@@ -40,16 +42,20 @@ options:
   role_arn:
     description:
       - ARN of the role to use for the S3 bucket
-    required: true
+    default: null
   bucket:
     description:
       - S3 bucket where repository will be saved
-    required: true
+    default: null
   snapshot_repository_name:
     description:
       - Name of the repository where snapshots will be saved
     default: null
     required: true
+  snapshot_name:
+    description:
+      - When taken snapshot, the name for the snapshot
+    default: null
   aws_access_key:
     description:
       - AWS access key to sign the requests.
@@ -67,7 +73,7 @@ requirements:
 
 EXAMPLES = '''
 
-- register_snapshot_repository:
+- aws_elasticsearch_snapshot:
     region: "eu-west-1"
     aws_access_key: "AKIAJ5CC6CARRKOX5V7Q"
     aws_secret_key: "cfDKFSXEo1CC6gfhfhCARRKOX5V7Q"
@@ -75,6 +81,14 @@ EXAMPLES = '''
     role_arn: "arn:aws:iam::1234456778:role/S3ElasticSearchLogs"
     bucket: "logs"
     snapshot_repository_name: "s3snapshots"
+
+- aws_elasticsearch_snapshot:
+    region: "eu-west-1"
+    aws_access_key: "AKIAJ5CC6CARRKOX5V7Q"
+    aws_secret_key: "cfDKFSXEo1CC6gfhfhCARRKOX5V7Q"
+    elasticsearch_host: "logs-q213lkjalsjda.eu-west-1.es.amazonaws.com"
+    snapshot_repository_name: "s3snapshots"
+    snapshot_name: "2016-02-01"
 '''
 try:
     import boto.ec2
@@ -82,12 +96,48 @@ try:
 except ImportError:
     HAS_BOTO=False
 
+def get_elasticsearch_connection(aws_access_key, aws_secret_key, region, host):
+    return ESConnection(aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region=region, host=host, is_secure=False)
+
+
+def create_snapshot_repository(es_connection, repository_name, bucket, region, role_arn):
+    return es_connection.make_request(
+            method='POST',
+            path='/_snapshot/' + repository_name,
+            data='{"type": "s3", "settings": { "bucket": "' + bucket + '", "region": "' + region + '", "role_arn": "' + role_arn + '"}}')
+
+
+def take_snapshot(es_connection, repository_name, snapshot_name):
+    return es_connection.make_request(
+            method='PUT',
+            path='/_snapshot/' + repository_name + '/' + snapshot_name)
+
+def do_main(module):
+    repository_name = module.params.get('repository_name')
+    snapshot_name = module.params.get('snapshot_name')
+    bucket = module.params.get('bucket')
+    region = module.params.get('region')
+    role_arn = module.params.get('role_arn')
+    aws_access_key = module.params.get('aws_access_key')
+    aws_secret_key = module.params.get('aws_secret_key')
+    host = module.params.get('host')
+
+    es_connection = get_elasticsearch_connection(aws_access_key, aws_secret_key, region, host)
+
+    if bucket and role_arn:
+        return create_snapshot_repository(es_connection, repository_name, bucket, region, role_arn)
+    elif snapshot_name:
+        return take_snapshot(es_connection, repository_name, snapshot_name)
+    else:
+        raise ValueError('Required parameters are missing')
+
 def main():
     argument_spec = ec2_argument_spec()
     argument_spec.update(dict(
             host = dict(required=True, aliases = ['elasticsearch_host']),
-            role_arn = dict(required=True),
-            bucket = dict(required=True),
+            role_arn = dict(),
+            bucket = dict(),
+            snapshot_name = dict(),
             repository_name = dict(required=True, aliases = ['snapshot_repository_name']),
         )
     )
@@ -100,16 +150,9 @@ def main():
         module.fail_json(msg='boto required for this module, install via pip or your package manager')
 
     try:
-        aws_auth_connection = ESConnection(aws_access_key_id=module.params.get('aws_access_key'), aws_secret_access_key=module.params.get('aws_secret_key'), region=module.params.get('region'), host=module.params.get('host'), is_secure=False)
+        module.exit_json(changed=True, resp=do_main(module).read())
     except (boto.exception.NoAuthHandlerFound, StandardError), e:
         module.fail_json(msg=str(e))
-
-    resp = aws_auth_connection.make_request(
-            method='POST',
-            path='/_snapshot/' + module.params.get('repository_name'),
-            data='{"type": "s3", "settings": { "bucket": "' + module.params.get('bucket') + '", "region": "' + module.params.get('region') + '", "role_arn": "' + module.params.get('role_arn') + '"}}')
-
-    module.exit_json(changed=True, resp=resp.read())
 
 
 # import module snippets
